1cd65d74df51b2bf11a638a26278ca8a01fcc12a
elreco <elreco@users.noreply.github.com>
2025-06-07 19:00:44 +0200

    Update src/App.tsx - Version 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { QRCodeGenerator } from "./components/QRCodeGenerator";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-50">
-      <h1 className="text-3xl font-bold mb-8 text-center">Dynamic QR Code Generator</h1>
-      <QRCodeGenerator />
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow flex flex-col items-center justify-center p-4 bg-gray-50">
+        <h1 className="text-3xl font-bold mb-8 text-center">Dynamic QR Code Generator</h1>
+        <QRCodeGenerator />
+      </main>
+      <Footer />
     </div>
   );
 }
